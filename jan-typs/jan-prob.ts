0eccc090b4ea5eaca2bbca2f4f911a7678b98201
Matthew Foley <matthewfoley333@gmail.com>
2026-02-22 22:50:56 +1300

    add the function signature

--- jan-typs/jan-prob.ts
+++ jan-typs/jan-prob.ts
@@ -631,3 +631,11 @@ export const quickKMostFreqElements = (
 
   return cleanArr
 }
+
+export const anotherQuickKMostFreqElements = (
+  numbers: number[],
+  k: number
+): number[] | null => {
+  //sanity check
+  if (numbers.length === 0 || typeof k === 'number') return null
+}
